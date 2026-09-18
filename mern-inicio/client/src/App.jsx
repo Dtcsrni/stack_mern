a3af46162d46e052a7354bbe889862e09ccdaa34
ErickV <armsystechno@gmail.com>
2026-09-18 08:24:48 -0600

    feat: implementar componente Encabezado y contador en App

--- mern-inicio/client/src/App.jsx
+++ mern-inicio/client/src/App.jsx
@@ -1,17 +1,18 @@
+import {useState} from "react";
 import "./App.css";
+import Encabezado from "./components/Encabezado.jsx";
 
 function App(){
-  const materia = "Diseño y Desarrollo de Apps Web";
-  const parcial = 3;
+  const [contador, setContador] = useState(0);
   return (
-    <main>
-      <h1>Gestor de tareas </h1>
-      <p>Aplicacion en React</p>
-      <p>Materia: {materia}</p>
-      <p>Parcial: {parcial}</p>
-      <p>4x5 = {4 * 5}</p>
-
-    </main>
+  <main>
+    <Encabezado 
+    titulo="Gestor de Tareas de septiembre CUH"
+    subtitulo="Aplicacion desarrollada en CUH con React en septiembre"
+    />
+    <p>Contador: {contador}</p>
+    <button onClick={() => setContador(contador + 1)}>Incrementar</button>
+  </main>
   );
 }
 
